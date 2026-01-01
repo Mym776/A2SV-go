d90c283e318c98766078ad4cdd2c8dc2634edf4d
Mym776 <Mymhs7@gmail.com>
2026-01-01 22:49:54 +0300

    cm

--- task/task8/task_manager_api/Infrastructure/auth_middleware.go
+++ task/task8/task_manager_api/Infrastructure/auth_middleware.go
@@ -10,7 +10,7 @@ import (
 )
 
 func middleware(c *gin.Context) (jwt.MapClaims, bool) {
-	
+	//middleware for auth
 	// checks if the auth is in the proper format
 	authHandler := c.GetHeader("Authorization")
 	if authHandler == "" {
